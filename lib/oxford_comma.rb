3cabec18c6ea92412a2bc5fe684eca96c7e12201
Alethia Quintero <alethiaq22@gmail.com>
2019-07-15 20:03:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+length = array.length
+case length
+when  1
+  return array.join
+when 2
+  return array.join(" and ")
+else
+  sentence = ""
+  last = array.pop
+  sentence << array.join(", ") + ", and " + last.to_s
+  return sentence
+end
 end
