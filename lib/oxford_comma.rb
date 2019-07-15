4f6f3862ea9c365cb498eecf00538518dc7fa660
Alethia Quintero <alethiaq22@gmail.com>
2019-07-15 19:56:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-  my_string = ""
-array(0..1).each do |element|
-    my_string << element.to_s + ", "
+length = array.length
+case length
+when  1
+  return array
+when 2
+  return array.join(", and")
+when 3
+  return array(0..1).join(", ") + ", and" + array.last.to_s
 end
-last = array.pop
-my_string << ", and "
-my_string << last
-return my_string
 end
